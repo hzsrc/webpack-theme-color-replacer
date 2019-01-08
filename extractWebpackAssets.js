d2246e368fbea89f7f7768bbd8f64b0359fa604b
huangzheng <zheng.huang@flaginfo.com.cn>
2019-01-08 15:16:12 +0800

    debug

--- extractWebpackAssets.js
+++ extractWebpackAssets.js
@@ -28,27 +28,27 @@
 
 var Reg_Lf_Rem = /\\\\?n|\/\*[\s\S]+\*\//g; // \n和备注
 module.exports = function extractAssets(assets, extractor) {
-    var isDebug = process.env.NODE_ENV == 'development';
-    var CssCodeReg = isDebug
-        //css-loader 2: \n// Module\nexports.push([module.i, \"a{   ...... }\\n\", \"\",{\"version\":3,
-        //css-loader 1: \n// module\nexports.push([module.i, \"a{   ...... }\\n\", \"\"]);
-        ? /\\nexports\.push\(\[module\.i, \\"(.+?\})(?:\\\\n)?\\", \\"\\"(\]\)|,\{)/g
-        : /\.push\(\[\w+\.i,['"](.+?\})\\n['"],['"]['"]\]\)/g; // from css-loader:  n.exports=t("FZ+f")(!1)).push([n.i,"\n.payment-type[data-v-ffb10066] {......}\n",""])
+  var isDebug = process.env.NODE_ENV == 'development' || process.argv.find(arg => arg.match(/\bdev/));
+  var CssCodeReg = isDebug
+    //css-loader 2: \n// Module\nexports.push([module.i, \"a{   ...... }\\n\", \"\",{\"version\":3,
+    //css-loader 1: \n// module\nexports.push([module.i, \"a{   ...... }\\n\", \"\"]);
+    ? /\\nexports\.push\(\[module\.i, \\"(.+?\})(?:\\\\n)?\\", \\"\\"(\]\)|,\s*\{)/g
+    : /\.push\(\[\w+\.i,['"](.+?\})\\n['"],['"]['"]\]\)/g; // from css-loader:  n.exports=t("FZ+f")(!1)).push([n.i,"\n.payment-type[data-v-ffb10066] {......}\n",""])
 
-    var cssSrcs = [];
-    Object.keys(assets).map(fn => {
-        if (fn.match(/\.css/i)) {
-            var src = assets[fn].source();
-            [].push.apply(cssSrcs, extractor.extractColors(src))
-        }
-        else if (fn.match(/\.js/i)) {
-            var src = assets[fn].source()
-            src.replace(CssCodeReg, (match, $1) => {
-                var modSrc = $1.replace(Reg_Lf_Rem, '');
-                cssSrcs.push.apply(cssSrcs, extractor.extractColors(modSrc))
-            })
-        }
-    });
-    return cssSrcs
+  var cssSrcs = [];
+  Object.keys(assets).map(fn => {
+    if (fn.match(/\.css/i)) {
+      var src = assets[fn].source();
+      [].push.apply(cssSrcs, extractor.extractColors(src))
+    }
+    else if (fn.match(/\.js/i)) {
+      var src = assets[fn].source()
+      src.replace(CssCodeReg, (match, $1) => {
+        var modSrc = $1.replace(Reg_Lf_Rem, '');
+        cssSrcs.push.apply(cssSrcs, extractor.extractColors(modSrc))
+      })
+    }
+  });
+  return cssSrcs
 }
 
