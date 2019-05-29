f218318310a76931d8257bbed0f2ff71b77c7b8c
hz <hz932@qq.com>
2019-05-29 09:12:11 +0000

    Update getElementUISeries.js

--- forElementUI/getElementUISeries.js
+++ forElementUI/getElementUISeries.js
@@ -1,6 +1,6 @@
 var varyColor = require('../client/varyColor')
 module.exports = function (colorStr) {
-    if (colorStr[0] = '#') colorStr = colorStr.slice(1)
+    if (colorStr[0] === '#') colorStr = colorStr.slice(1)
     var colors = ['#' + colorStr, varyColor.toNum3(colorStr).join(',')]
     for (var i = 0; i <= 9; i++)
         colors.push(varyColor.lighten(colorStr, Number((i / 10).toFixed(2))));
