f859b2cf000340530a56aeb6d5d17fa60ce559ff
huangzheng <zheng.huang@flaginfo.com.cn>
2019-09-16 10:49:07 +0800

    测试 #f67a1750 这样的颜色

--- test/nodejs/index.js
+++ test/nodejs/index.js
@@ -1,13 +1,21 @@
-
 var AssetsExtract = require('../../src/AssetsExtractor')
 var Extractor = require('../../src/Extractor')
 var path = require('path')
 var fs = require('fs')
 var glob = require('glob')
+var mkdirp = require('mkdirp')
 var options = require('../options')
+var themeColorChanger = require('../../client/themeColorChanger')
+var getElementUISeries = require('../../forElementUI/getElementUISeries')
 
-var files = glob.sync(path.join(__dirname, 'output-by-webpack/*.*'))
-files.map(extractOne)
+
+function startRun() {
+    // js是否压缩过
+    options.isJsUgly = false
+
+    var files = glob.sync(path.join(__dirname, 'output-by-webpack/*.*'))
+    files.map(extractOne)
+}
 
 function extractOne(pathFn) {
     var fn = path.basename(pathFn)
@@ -15,8 +23,17 @@ function extractOne(pathFn) {
 
     var ret = new AssetsExtract(options).extractAsset(fn, {source: t => content})
     var code = ret.join('\n')
-    var outFile = path.join(__dirname, '../dist/css/' + fn + '.css')
+    var outFile = path.join(__dirname, './css/' + fn + '.css')
+
+    mkdirp.sync(path.dirname(outFile))
+
     fs.writeFileSync(outFile, code)
     console.log('Output length:', code.length, '\n' + outFile)
-}
 
+
+    var newColors = getElementUISeries('#bd3be7')
+    var replacedCss = themeColorChanger.replaceCssText(code, options.matchColors, newColors)
+
+    var replacedFn = path.join(__dirname, './css/' + fn + '-new.css')
+    fs.writeFileSync(replacedFn, replacedCss)
+}
--- test/options.js
+++ test/options.js
@@ -1,7 +1,9 @@
+var getElementUISeries = require('../forElementUI/getElementUISeries')
+
 module.exports = {
     isJsUgly: true,
     fileName: 'css/theme-colors.[contenthash:8].css',
-    matchColors: '#f67a17|246,122,23|#f67a17|#f7872e|#f89545|#f9a25d|#faaf74|#fbbd8b|#fbcaa2|#fcd7b9|#fde4d1|#fef2e8|#dd6e15|#c56212'.split('|'), // 主色系列
+    matchColors: getElementUISeries('#f67a17'), // 主色系列 #f67a17|246,122,23|#f67a17|#f7872e|#f89545|#f9a25d|#faaf74|#fbbd8b|#fbcaa2|#fcd7b9|#fde4d1|#fef2e8|#dd6e15|#c56212'.split('|')
     // 改变样式选择器，解决样式覆盖问题
     changeSelector(selector) {
         switch (selector) {
