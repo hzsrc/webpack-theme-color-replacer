2c86c3aab8bacb588213b9c738575a25959d44bb
hz <hz932@qq.com>
2021-05-09 21:35:54 +0800

    增加测试用例

--- test/options.js
+++ test/options.js
@@ -18,7 +18,8 @@ module.exports = {
             '#fde4d1', '#fff5e6', '#ffe2ba', '#ffce91',
             '#ffb669', '#ff9c40', '#f67a17', '#cf5b08',
             '#a84100', '#822e00', '#5c1d00',
-            '246, 122, 23', '27, 92.531%, 52.745%'
+            '246, 122, 23', '27, 92.531%, 52.745%',
+            '#222', '#222223', '#ADF'
         ],
         newColors: [
             '#bd3be7', '#c44fe9', '#ca62ec', '#d176ee',
@@ -26,13 +27,14 @@ module.exports = {
             '#f2d8fa', '#fef0ff', '#fce6ff', '#f5bdff',
             '#eb94ff', '#d667f5', '#bd3be7', '#9629c2',
             '#72199c', '#510d75', '#34084f',
-            '189, 59, 231', '285,78.182%,56.863%'
+            '189, 59, 231', '285,78.182%,56.863%',
+            '#333', '#333334', '#6ED'
         ],
     },
     build: {
         isJsUgly: true,
         fileName: 'css/theme-colors.[contenthash:8].css',
-        matchColors: [...getElementUISeries('#f67a17'), '#222', '#222223'], // 主色系列 #f67a17|246,122,23|#f67a17|#f7872e|#f89545|#f9a25d|#faaf74|#fbbd8b|#fbcaa2|#fcd7b9|#fde4d1|#fef2e8|#dd6e15|#c56212'.split('|')
+        matchColors: [...getElementUISeries('#f67a17'), '#222', '#222223', '#ADF'], // 主色系列 #f67a17|246,122,23|#f67a17|#f7872e|#f89545|#f9a25d|#faaf74|#fbbd8b|#fbcaa2|#fcd7b9|#fde4d1|#fef2e8|#dd6e15|#c56212'.split('|')
         // 改变样式选择器，解决样式覆盖问题
         changeSelector(selector, util) {
             switch (selector) {
@@ -43,7 +45,7 @@ module.exports = {
             }
         },
         externalCssFiles: require('path').join(__dirname, './external.css'),
-        newColors: [...getElementUISeries('#bd3be7'), '#333', '#333334'],
+        newColors: [...getElementUISeries('#bd3be7'), '#333', '#333334', '#6ED'],
     },
 }
 
